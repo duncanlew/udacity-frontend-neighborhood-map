fd2c33367a0ea5e8b77cc27cb0cbbf8558663379
Duncan <duncan@lew.email>
2018-12-27 21:18:41 +0100

    shortening with jquery

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,6 @@
 let map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map($('#map')[0], {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
