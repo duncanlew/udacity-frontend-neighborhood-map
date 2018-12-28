eeebf7469230e639691610b432534956163413ba
Duncan <duncan@lew.email>
2018-12-28 19:38:34 +0100

    navbar (heading) fixed

--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,15 @@
 let map;
+
 function initMap() {
+    let headingHeight = $("#heading").outerHeight(true);
+    let windowHeight = $(window).outerHeight(true);
+    let mapsHeight = windowHeight - headingHeight;
+    $("#map").height(mapsHeight);
     map = new google.maps.Map($('#map')[0], {
-        center: {lat: -34.397, lng: 150.644},
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
         zoom: 8
     });
 }
