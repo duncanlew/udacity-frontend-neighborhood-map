32e1cd82e2794b8967f03449819ece389de8de8c
Duncan <duncan@lew.email>
2019-01-03 21:31:03 +0100

    moving of stuff to locations.js

--- js/maps.js
+++ js/maps.js
@@ -9,77 +9,6 @@ let startingLocation = {
     },
     zoom: 13
 }
-let locations = [{
-        title: 'De Haagse Markt',
-        position: {
-            lat: 52.06503762916279,
-            lng: 4.299366977145978
-        }
-    },
-    {
-        title: 'Madurodam',
-        position: {
-            lat: 52.09908797895354,
-            lng: 4.298863363483052
-        }
-    },
-    {
-        title: 'Mauritshuis',
-        position: {
-            lat: 52.080303656366404,
-            lng: 4.314457251614486
-        }
-    },
-    {
-        title: 'Peace Palace',
-        position: {
-            lat: 52.08611889900742,
-            lng: 4.295209836563272
-        }
-    },
-    {
-        title: 'Binnenhof',
-        position: {
-            lat: 52.079457031469644,
-            lng: 4.312365524934367
-        }
-    },
-    {
-        title: 'Escher in het Paleis',
-        position: {
-            lat: 52.083381407262536,
-            lng: 4.314079464728019
-        }
-    },
-    {
-        title: 'Gemeentemuseum Den Haag',
-        position: {
-            lat: 52.08928982328055,
-            lng: 4.280290603637695
-        }
-    },
-    {
-        title: 'Paleis Noordeinde',
-        position: {
-            lat: 52.080764,
-            lng: 4.307623
-        }
-    },
-    {
-        title: 'De Pier',
-        position: {
-            lat: 52.117825003805194,
-            lng: 4.279882607795804
-        }
-    },
-    {
-        title: 'Omniversum',
-        position: {
-            lat: 52.08937727145052,
-            lng: 4.281729695781964
-        }
-    },
-];
 
 
 function initMap() {
@@ -125,7 +54,7 @@ function setMapCorrectWidth(isSidebarClosed) {
         $("#map").width(mapWidth);
         google.maps.event.trigger(map, "resize");
     }
-    
+
 }
 
 function createMarkers() {
@@ -216,6 +145,6 @@ function getAddress(marker) {
     });
 }
 
-function failedInitMaps(){
+function failedInitMaps() {
     alert("Unable to load Google Maps!");
 }
